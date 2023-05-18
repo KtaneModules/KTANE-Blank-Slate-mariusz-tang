650cac939a880964c3ab8e92479af6e0883dfcf9
TheKuroEver <mariusztang@outlook.com>
2023-05-18 18:11:21 +0100

    Purge unnecessary materials

--- Assets/_BlankSlates/_Scripts/RuleStates/PunctuationMarksState.cs
+++ Assets/_BlankSlates/_Scripts/RuleStates/PunctuationMarksState.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class PunctuationMarksState : RuleStateController {
 
+    private const string COLOUR_ORDER = "ROYGBPKW";
     private readonly Vector3 _buttonScale = new Vector3(0.0002f, 0.00007f, 0.0002f);
+    private readonly Vector3 _glitchScaleSmall = new Vector3(0.3839059f, 0.02574724f, 0.3839059f);
+    private readonly Vector3 _glitchScaleBig = new Vector3(0.8879244f, 0.05955003f, 0.8879244f);
 
-    [SerializeField] private KMSelectable[] _buttons;
     [SerializeField] private Color[] _glitchColours;
     [SerializeField] private Transform _glitchParentTransform;
     [SerializeField] private SpriteRenderer[] _glitchSquares;
 
+    [SerializeField] private KMSelectable[] _logicDiveButtons;
+    [SerializeField] private Color[] _logicDiveColours;
+
     private void Start() {
-        foreach (KMSelectable button in _buttons) {
+        foreach (KMSelectable button in _logicDiveButtons) {
             button.GetComponent<MeshRenderer>().enabled = true;
             button.transform.localScale = Vector3.zero;
         }
@@ -24,6 +29,7 @@ public class PunctuationMarksState : RuleStateController {
 
     public override IEnumerator OnStateEnter(Region pressedRegion) {
         _glitchParentTransform.position = pressedRegion.transform.position;
+        _glitchParentTransform.localScale = _glitchScaleSmall;
         yield return StartCoroutine(GlitchAnimation());
         yield return null;
     }
