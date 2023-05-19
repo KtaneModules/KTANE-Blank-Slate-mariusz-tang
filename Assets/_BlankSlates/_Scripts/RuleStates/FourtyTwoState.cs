049d29ec322d084ff7646882b082d8fc292598ab
TheKuroEver <mariusztang@outlook.com>
2023-05-19 13:55:26 +0100

    Implement 42 rule

---
+++ Assets/_BlankSlates/_Scripts/RuleStates/FourtyTwoState.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Rnd = UnityEngine.Random;
+
+public class FourtyTwoState : RuleStateController {
+
+    [SerializeField] private Color[] _colours;
+    [SerializeField] private TextMesh[] _textMeshes;
+
+    private int _targetRegionNumber;
+    private int[][] _numberSequences;
+    private Coroutine _cycling;
+
+    public override IEnumerator OnStateEnter(Region pressedRegion) {
+        _targetRegionNumber = _module.AvailableRegions.PickRandom();
+        _numberSequences = GenerateNumberSequences(_targetRegionNumber);
+        _cycling = StartCoroutine(CycleNumbers());
+
+        _module.Log("Numbers have started cycling on each region.");
+        _module.Log($"Region {_targetRegionNumber}'s sequence contains 42. Press this region.");
+
+        yield return null;
+    }
+
+    private int[][] GenerateNumberSequences(int targetRegionNumber) {
+        var numberSequences = new int[8][];
+
+        for (int i = 0; i < 8; i++) {
+            numberSequences[i] = new int[10];
+            for (int j = 0; j < 10; j++) {
+                int newNumber = Rnd.Range(0, 99);
+                if (newNumber >= 42) {
+                    newNumber++;
+                }
+                numberSequences[i][j] = newNumber;
+            }
+        }
+
+        int fourtyTwoPosition = Rnd.Range(0, 10);
+        numberSequences[targetRegionNumber - 1][fourtyTwoPosition] = 42;
+
+        return numberSequences;
+    }
+
+    private IEnumerator CycleNumbers() {
+        _module.BombAudio.PlaySoundAtTransform("42 Hold", transform);
+        int j = 0;
+        while (true) {
+            for (int i = 0; i < 8; i++) {
+                _textMeshes[i].text = $"{_numberSequences[i][j]:00}";
+                _textMeshes[i].color = _colours.PickRandom();
+            }
+            yield return new WaitForSeconds(1);
+            j++;
+            j %= 10;
+        }
+    }
+
+    public override IEnumerator HandleRegionPress(Region pressedRegion) {
+        int pressedNumber = pressedRegion.Number;
+
+        if (pressedNumber == _targetRegionNumber) {
+            StopCoroutine(_cycling);
+            Array.ForEach(_textMeshes, t => t.text = string.Empty);
+            _module.Log("Pressed the correct region!");
+            // ! _module.GetNewState(pressedRegion);
+            _module.Log("Correct!");
+        }
+        else {
+            _module.Strike($"Incorrectly pressed region {pressedNumber}. Strike!");
+        }
+
+        yield return null;
+    }
+
+}
